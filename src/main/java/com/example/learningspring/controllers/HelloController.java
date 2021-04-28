639a422377cbc40f1da76afdfcd3971fc4738683
audiesha brattin <a.brattin@gmail.com>
2021-04-28 10:11:45 -0500

    adding

--- src/main/java/com/example/learningspring/controllers/HelloController.java
+++ src/main/java/com/example/learningspring/controllers/HelloController.java
@@ -1,9 +1,7 @@
 package com.example.learningspring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 class HelloController {
@@ -12,9 +10,18 @@ class HelloController {
   public String hello() {
     return "Hello from Spring!";
   }
+
   @GetMapping("/hello/{name}")
   @ResponseBody
   public String sayHello(@PathVariable String name) {
     return "Hello " + name + "!";
+  }
+
+//request mapping and get mapping do the same thing.
+//Notice in the above example we also used the @RequestMapping annotation, which is just the longer version of @GetMapping. There, of course, also exists a @PostMapping annotation that tells the controller to respond to POST requests.
+  @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+  @ResponseBody
+  public String addOne(@PathVariable int number) {
+    return number + " plus one is " + (number + 1) + "!";
   }
 }
