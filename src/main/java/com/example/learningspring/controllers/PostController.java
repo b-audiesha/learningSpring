daa861f377d3882b21e713d550ce25e3201d6429
audiesha brattin <a.brattin@gmail.com>
2021-04-28 11:59:35 -0500

    adding Post Control

--- src/main/java/com/example/learningspring/controllers/PostController.java
+++ src/main/java/com/example/learningspring/controllers/PostController.java
@@ -1,15 +1,31 @@
 package com.example.learningspring.controllers;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class PostController {
-  @GetMapping("/post")
+  @RequestMapping(path = "/post", method = RequestMethod.GET)
   @ResponseBody
-  public String hello() {
-    return "Hello from Spring!";
+  public String index() {
+    return "index page";
+  }
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String PostID() {
+      return "view an individual post\n";
+    }
+  @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+  @ResponseBody
+  public String create() {
+    return "creation form here";
+  }
+  @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+  @ResponseBody
+  public String post() {
+    return "creation form here";
   }
 }
 
