1dc01603e8358ebd16314340d702c4aa329c5831
audiesha brattin <a.brattin@gmail.com>
2021-04-29 09:53:20 -0500

    adding thymeleaf

--- src/main/java/com/example/learningspring/controllers/HelloController.java
+++ src/main/java/com/example/learningspring/controllers/HelloController.java
@@ -1,24 +1,26 @@
 package com.example.learningspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 class HelloController {
-  @GetMapping("/hello")
+  @GetMapping("/hello")  //does this equal hello.html?
   @ResponseBody
   public String hello() {
     return "Hello from Spring!";
   }
 
   @GetMapping("/hello/{name}")
-  @ResponseBody
-  public String sayHello(@PathVariable String name) {
-    return "Hello " + name + "!";
+  public String sayHello(@PathVariable String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello";
   }
 
 //request mapping and get mapping do the same thing.
 //Notice in the above example we also used the @RequestMapping annotation, which is just the longer version of @GetMapping. There, of course, also exists a @PostMapping annotation that tells the controller to respond to POST requests.
+
   @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
   @ResponseBody
   public String addOne(@PathVariable int number) {
--- src/main/java/com/example/learningspring/controllers/HomeController.java
+++ src/main/java/com/example/learningspring/controllers/HomeController.java
@@ -2,12 +2,12 @@ package com.example.learningspring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HomeController {
-  @GetMapping("/")
-  @ResponseBody
-  public String landing(){
-    return "This is the landing page!";
+
+  @GetMapping("/home")
+  public String welcome() {
+    return "home";
   }
 }
