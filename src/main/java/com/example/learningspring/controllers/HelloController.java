ab689d69ee3584970cddc9e42448e63d00e67158
audiesha brattin <a.brattin@gmail.com>
2021-05-02 12:40:00 -0500

    updating Hello Controller

--- src/main/java/com/example/learningspring/controllers/HelloController.java
+++ src/main/java/com/example/learningspring/controllers/HelloController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 class HelloController {
-  @GetMapping("/hello")  //does this equal hello.html?
+
+  @GetMapping("/hello")
   @ResponseBody
   public String hello() {
     return "Hello from Spring!";
@@ -16,6 +17,12 @@ class HelloController {
   public String sayHello(@PathVariable String name, Model model) {
     model.addAttribute("name", name);
     return "hello";
+  }
+
+  @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+  @ResponseBody
+  public String addOne(@PathVariable int number) {
+    return number + " plus one is " + (number + 1) + "!";
   }
 
   @GetMapping("/join")
@@ -28,12 +35,39 @@ class HelloController {
     model.addAttribute("cohort", "Welcome to " + cohort + "!");
     return "join";
   }
-//request mapping and get mapping do the same thing.
-//Notice in the above example we also used the @RequestMapping annotation, which is just the longer version of @GetMapping. There, of course, also exists a @PostMapping annotation that tells the controller to respond to POST requests.
-
-  @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-  @ResponseBody
-  public String addOne(@PathVariable int number) {
-    return number + " plus one is " + (number + 1) + "!";
-  }
 }
+
+
+//@Controller
+//class HelloController {
+//  @GetMapping("/hello")  //does this equal hello.html?
+//  @ResponseBody
+//  public String hello() {
+//    return "Hello from Spring!";
+//  }
+//
+//  @GetMapping("/hello/{name}")
+//  public String sayHello(@PathVariable String name, Model model) {
+//    model.addAttribute("name", name);
+//    return "hello";
+//  }
+//
+//  @GetMapping("/join")
+//  public String showJoinForm() {
+//    return "join";
+//  }
+//
+//  @PostMapping("/join")
+//  public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+//    model.addAttribute("cohort", "Welcome to " + cohort + "!");
+//    return "join";
+//  }
+////request mapping and get mapping do the same thing.
+////Notice in the above example we also used the @RequestMapping annotation, which is just the longer version of @GetMapping. There, of course, also exists a @PostMapping annotation that tells the controller to respond to POST requests.
+//
+//  @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+//  @ResponseBody
+//  public String addOne(@PathVariable int number) {
+//    return number + " plus one is " + (number + 1) + "!";
+//  }
+//}
--- src/main/java/com/example/learningspring/controllers/HomeController.java
+++ src/main/java/com/example/learningspring/controllers/HomeController.java
@@ -2,12 +2,14 @@ package com.example.learningspring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HomeController {
-
-  @GetMapping("/home")
-  public String welcome() {
-    return "home";
+  @GetMapping("/")
+  @ResponseBody
+  public String landing() {
+    return "This is the landing page";
   }
 }
