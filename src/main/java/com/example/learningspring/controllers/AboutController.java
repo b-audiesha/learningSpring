d6a96f1b25c377600504743da00dcbdd8efbf815
audiesha brattin <a.brattin@gmail.com>
2021-05-03 11:51:46 -0500

    adding changes

--- src/main/java/com/example/learningspring/controllers/AboutController.java
+++ src/main/java/com/example/learningspring/controllers/AboutController.java
@@ -1,15 +1,14 @@
 package com.example.learningspring.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
-public class HomeController {
-  @GetMapping("/home")
-  @ResponseBody
-  public String home() {
-    return "home";
-  } // this is the thymeleaf viewer equivalent to home.html
-}
+public class AboutController {
+  @GetMapping("/about")
+  public String about() {
+        return "about";
+      } // this is the thymeleaf viewer equivalent to home.html
+    }
+
+
--- src/main/java/com/example/learningspring/controllers/PostController.java
+++ src/main/java/com/example/learningspring/controllers/PostController.java
@@ -2,31 +2,30 @@ package com.example.learningspring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
 @Controller
 public class PostController {
 
   @GetMapping("/posts")
   @ResponseBody
-  public String postsIndex() {
+  public String index() {
     return "posts index page";
   }
 
   @GetMapping("/posts/{id}")
   @ResponseBody
-  public String postView() {
+  public String view() {
     return "view an individual post";
   }
 
   @GetMapping("/posts/create")
   @ResponseBody
-  public String postForm() {
+  public String create() {
     return "view the form for creating a post";
   }
 
   @PostMapping("/posts/create")
   @ResponseBody
-  public String createPost() {
+  public String newPost() {
     return "creating a new post";
   }
 }
